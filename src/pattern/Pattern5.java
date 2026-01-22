a4aa80ec3e8c118ebe4b4dfcdfad48422e401f59
uixPhuke <uixphuke@gmail.com>
2026-01-23 00:37:36 +0530

    pattern 5

---
+++ src/pattern/Pattern5.java
@@ -0,0 +1,17 @@
+package pattern;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+            pattern5(5);
+    }
+
+    static void pattern5(int n) {
+        for (int row = 1; row <=2*n; row++) {
+            int totalRow=row>n?2*n-row:row;
+            for (int col = 1; col <=totalRow; col++) {
+                System.out.print("X ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
