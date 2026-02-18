99365ea96c7571e7082199ba39eae01dfe4edf29
uixPhuke <uixphuke@gmail.com>
2026-02-18 23:48:54 +0530

    not in fibonacci

--- src/Basic/Fraction.java
+++ src/Basic/Fraction.java
@@ -1,4 +1,21 @@
 package Basic;
 
-public class Fraction {
+class Fraction {
+    int num;
+    int den;
+
+    // Default constructor
+    Fraction() {
+        num = 0;
+        den = 1;
+    }
+
+    void show() {
+        System.out.println(num + "/" + den);
+    }
+
+    public static void main(String[] args) {
+        Fraction f = new Fraction();
+        f.show();
+    }
 }
--- src/Basic/NotFibonacci.java
+++ src/Basic/NotFibonacci.java
@@ -1,4 +1,34 @@
 package Basic;
 
-public class NotFibonacci {
+import java.util.Scanner;
+
+class NotFibonacci {
+
+    // Check perfect square
+    static boolean isPerfectSquare(int x) {
+        int s = (int) Math.sqrt(x);
+        return s * s == x;
+    }
+
+    // Check Fibonacci number
+    static boolean isFibonacci(int n) {
+        return isPerfectSquare(5 * n * n + 4) ||
+                isPerfectSquare(5 * n * n - 4);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the range: ");
+        int N = sc.nextInt();
+
+        System.out.println("Numbers NOT in Fibonacci series:");
+        for (int i = 1; i <= N; i++) {
+            if (!isFibonacci(i)) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
 }
