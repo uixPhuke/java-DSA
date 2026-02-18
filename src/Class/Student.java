6c0227ee100fb2a084e27c2e7015b8a31711d23e
uixPhuke <uixphuke@gmail.com>
2026-02-18 23:49:24 +0530

    stusent --> contructor

---
+++ src/Class/Student.java
@@ -0,0 +1,39 @@
+package Class;
+
+class Student {
+
+    //  Instance variables
+    int roll;
+    String name;
+
+    // Static variable
+    static String college = "KIIT";
+
+    // Constructor
+    Student(int roll, String name) {
+        this.roll = roll;     // this → instance variable
+        this.name = name;
+    }
+
+    //  Instance method
+    void display() {
+        // Local variable
+        int year = 2026;
+
+        System.out.println("Roll   : " + roll);
+        System.out.println("Name   : " + name);
+        System.out.println("College: " + college);
+        System.out.println("Year   : " + year);
+        System.out.println("-------------------");
+    }
+
+    public static void main(String[] args) {
+
+        // Object creation
+        Student s1 = new Student(101, "Raj");
+        Student s2 = new Student(102, "Aman");
+
+        s1.display();
+        s2.display();
+    }
+}
