cac3ca3a1b390e1aab032cf4d756331eff7f200c
uixPhuke <uixphuke@gmail.com>
2026-03-02 18:00:00 +0530

    Password

---
+++ src/Basic/UserDefinePassword.java
@@ -0,0 +1,37 @@
+package Basic;
+
+import java.util.Scanner;
+
+    class UserDefinedPassword {
+        String password;
+
+        // Constructor to set password
+        UserDefinedPassword(String password) {
+            this.password = password;
+        }
+
+        // Method to check password
+        void checkPassword(String input) {
+            if (password.equals(input)) {
+                System.out.println("Correct Password");
+            } else {
+                System.out.println("Incorrect Password");
+            }
+        }
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Set your password: ");
+            String setPass = sc.nextLine();
+
+            UserDefinedPassword obj = new UserDefinedPassword(setPass);
+
+            System.out.print("Enter password to login: ");
+            String loginPass = sc.nextLine();
+
+            obj.checkPassword(loginPass);
+        }
+    }
+}
